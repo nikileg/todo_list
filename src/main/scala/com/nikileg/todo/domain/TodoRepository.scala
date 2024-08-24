8b5c071b74487b7e20023d1f8e7deaade25ac22d
nikileg <10009432+nikileg@users.noreply.github.com>
2024-08-24 16:19:53 +0300

    wip repo

--- src/main/scala/com/nikileg/todo/domain/TodoRepository.scala
+++ src/main/scala/com/nikileg/todo/domain/TodoRepository.scala
@@ -1,19 +1,13 @@
 package com.nikileg.todo.domain
 
-import cats.Monad
 import cats.effect.Sync
 import cats.effect.std.Console
+import cats.syntax.all._
+import com.nikileg.todo.domain.model.TodoList.ListId
 import com.nikileg.todo.domain.model.{TodoItem, TodoList}
 import doobie.Transactor
 import doobie.implicits._
-import cats.syntax.all._
-import com.nikileg.todo.domain.model.TodoList.ListId
-import doobie.enumerated.JdbcType
-import doobie.postgres._
 import doobie.postgres.implicits._
-import doobie.util.Get
-import doobie.util.invariant.InvalidObjectMapping
-import org.postgresql.util.PGobject
 import org.typelevel.log4cats.Logger
 
 import java.util.UUID
@@ -29,8 +23,8 @@ trait TodoRepository[F[_]] {
 }
 
 object TodoRepository {
-  private case class TodoDto(id: UUID, description: String, title: String, items: List[ItemDto]) {
-    def asModel: TodoList = TodoList(id, items.map(_.asModel).toIndexedSeq)
+  private case class TodoDto(id: UUID, description: String, title: String) {
+    def asModel(items: Seq[ItemDto]): TodoList = TodoList(id, items.map(_.asModel).toIndexedSeq)
   }
 
   private case class ItemDto(id: Int, listId: UUID, isChecked: Boolean, name: String, description: String) {
@@ -44,26 +38,27 @@ object TodoRepository {
     }
   }
 
-//  private implicit val itemDtoGet: Get[ItemDto] = Get[String].map(ItemDto.fromString)
-  private implicit val itemDtoArrayGet: Get[Array[ItemDto]] = Get[String].map(
-    _.stripPrefix("{").stripSuffix("}").split(",").map(
-      _.stripPrefix("(").stripSuffix(")")
-    ).map(ItemDto.fromString)
-  )
-
   def live[F[_] : Sync : Console : Logger](tx: Transactor[F]) = new TodoRepository[F] {
     override def getById(id: UUID): F[Option[TodoList]] = {
       val fr =
         sql"""
-         select t.id, t.description, t.title, array_agg(it.name)::text from todolist.todos t
-          left join todolist.items it on t.id = it.list_id
+         select t.id, t.description, t.title from todolist.todos t
           where t.id = $id
-          group by t.id, t.description, t.title
          """
-      fr.query[TodoDto].to[List].map(_.headOption).transact(tx).map(_.map(_.asModel)).handleErrorWith {
-        case e => Logger[F].error(e)("Failed to get TodoList") *>
-          Console[F].printStackTrace(e).as(None)
-      }
+      val fr2 =
+        sql"""
+          select it.id, it.list_id, it.is_checked, it.name, it.description
+           from todolist.items it
+           where it.list_id = $id
+           """
+      (for {
+        v1 <- fr.query[TodoDto].option
+        v2 <- fr2.query[ItemDto].to[List]
+      } yield (v1, v2))
+        .transact(tx).map(pair => pair._1.map(_.asModel(pair._2))).handleErrorWith {
+          case e => Logger[F].error(e)("Failed to get TodoList") *>
+            Console[F].printStackTrace(e).as(None)
+        }
     }
 
     override def remove(id: UUID): F[Boolean] = ???
